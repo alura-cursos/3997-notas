2deede355caa460067bbada42f10a014bfd90d94
ikyrie <mah.c5r@gmail.com>
2024-07-18 19:13:06 -0300

    video5.2

--- bin/notas.dart
+++ bin/notas.dart
@@ -1,9 +1,20 @@
+import 'dart:io';
+
 void main() {
-  
+  getComando();
 }
 
 String getComando() {
   print("Digite um comando: 1 - Adicionar nota, 2 - Listar notas, 3 - Sair");
   List<String> comandos = <String>["1", "2", "3"];
   String? entrada = "";
+
+  entrada = stdin.readLineSync();
+
+  if (entrada == null || !comandos.contains(entrada)) {
+    print("Comando inválido");
+    getComando();
+  }
+
+  return entrada!;
 }
