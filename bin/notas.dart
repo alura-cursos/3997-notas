e519663d0c0e6b796b23dc434696abcbb76299f1
ikyrie <mah.c5r@gmail.com>
2024-07-18 19:12:29 -0300

    video5.1

--- bin/notas.dart
+++ bin/notas.dart
@@ -1,5 +1,9 @@
-import 'package:notas/notas.dart' as notas;
+void main() {
+  
+}
 
-void main(List<String> arguments) {
-  print('Hello world: ${notas.calculate()}!');
+String getComando() {
+  print("Digite um comando: 1 - Adicionar nota, 2 - Listar notas, 3 - Sair");
+  List<String> comandos = <String>["1", "2", "3"];
+  String? entrada = "";
 }
